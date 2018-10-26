15725af5b49614f3822cd3c6fdbe33b486bd58bb
bhjeon76 <bhjeon@shmobile.co.kr>
2018-10-26 18:54:51 +0900

    add ORSSerialPort

---
+++ CommandLineScope-Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  CommandLineScope-Bridge-Header.h
+//  CommandLineScope
+//
+//  Created by 전병학 on 26/10/2018.
+//  Copyright © 2018 전병학. All rights reserved.
+//
+
+#ifndef CommandLineScope_Bridge_Header_h
+#define CommandLineScope_Bridge_Header_h
+
+#import "ORSSerialPort.h"
+#import "ORSSerialPortManager.h"
+
+#endif /* CommandLineScope_Bridge_Header_h */
--- CommandLineScope/main.swift
+++ CommandLineScope/main.swift
@@ -8,6 +8,50 @@
 
 import Foundation
 
+class SerialHandler : NSObject, ORSSerialPortDelegate {
+    
+    var serialPort: ORSSerialPort?
+    
+    // ORSSerialPortDelegate
+    
+    
+    
+    func serialPort(serialPort: ORSSerialPort, didReceiveData data: NSData) {
+        if let string = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) {
+            print("\(string)")
+        }
+    }
+    
+    func serialPortWasRemovedFromSystem(serialPort: ORSSerialPort) {
+        self.serialPort = nil
+    }
+    
+    func serialPortWasRemoved(fromSystem serialPort: ORSSerialPort) {
+        print("Serial port \(serialPort) was remove")
+    }
+   
+    func serialPort(_ serialPort: ORSSerialPort, didEncounterError error: Error) {
+        
+        print("Serial port (\(serialPort)) encountered error: \(error)")
+    }
+    
+    func serialPortWasOpened(_ serialPort: ORSSerialPort) {
+        
+        print("serialPortWasOpened() \(serialPort.name)")
+    }
+    
+    func serialPortWasClosed(_ serialPort: ORSSerialPort) {
+        
+        print("serialPortWasClosed() \(serialPort.name)")
+    }
+    
+    func serialPort(_ serialPort: ORSSerialPort, didReceive data: Data) {
+        
+        if let string = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) {
+            print("\(string)")
+        }
+    }
+}
 //print("Hello, World! Command Line Scope")
 
 let scopeShell = ScopeShell()
